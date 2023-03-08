b5bedd5b489d018df7d749fa6eb7bf1cfb000c47
zhmgczh <54766809+zhmgczh@users.noreply.github.com>
2023-03-08 08:56:42 +0800

    Add Service Worker.

---
+++ js/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('fetch', async (event) => {
+    const { request } = event;
+    let response = await fetch(request);
+    // 3.重新构造Response
+    response = new Response(response.body, response)
+    // 4.篡改响应头
+    response.headers.delete('Content-Security-Policy');
+    response.headers.delete('X-Frame-Options');
+
+    event.respondWith(Promise.resolve(originalResponse));
+});
