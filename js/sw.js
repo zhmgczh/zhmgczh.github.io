903799ee9118058db70bae3a5c83e1a9658e4920
zhmgczh <54766809+zhmgczh@users.noreply.github.com>
2023-03-08 08:58:05 +0800

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,8 +1,9 @@
-self.addEventListener('fetch', async (event) => {
-    const { request } = event;
-    let response = await fetch(request);
+self.addEventListener('fetch',async(event)=>
+{
+    const {request}=event;
+    let response=await fetch(request);
     // 3.重新构造Response
-    response = new Response(response.body, response)
+    response=new Response(response.body,response)
     // 4.篡改响应头
     response.headers.delete('Content-Security-Policy');
     response.headers.delete('X-Frame-Options');
